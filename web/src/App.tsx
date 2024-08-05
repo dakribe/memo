5bada713c84707825e2e2614b7ac74d9b98070a4
dakribe <dakribe94@gmail.com>
2024-08-04 20:14:58 -0500

    Setup solid router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
+import { Route, Router } from "@solidjs/router";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+
 function App() {
   return (
-    <>
-      <h1 class="text-white">Hello world</h1>
-    </>
+    <Router>
+      <Route path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Router>
   );
 }
 
