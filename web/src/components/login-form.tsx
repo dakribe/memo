2540d3a08727c98ed3a8f14ec2d858f3144bb9f0
dakribe <dakribe94@gmail.com>
2024-08-04 20:51:10 -0500

    Login form

---
+++ web/src/components/login-form.tsx
@@ -0,0 +1,51 @@
+import { createForm, SubmitHandler, valiForm } from "@modular-forms/solid";
+import * as v from "valibot";
+
+const LoginSchema = v.object({
+  email: v.pipe(
+    v.string(),
+    v.nonEmpty("Please enter your email"),
+    v.email("Enter a vaild email address"),
+  ),
+  password: v.pipe(
+    v.string(),
+    v.nonEmpty("Please enter your password"),
+    v.minLength(8, "Your password must be 8 characters or more"),
+  ),
+});
+
+type LoginForm = v.InferInput<typeof LoginSchema>;
+
+export function LoginForm() {
+  const [loginForm, { Form, Field }] = createForm<LoginForm>({
+    validate: valiForm(LoginSchema),
+  });
+
+  const handleLogin: SubmitHandler<LoginForm> = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Form onSubmit={handleLogin}>
+      <Field name="email">
+        {(field, props) => (
+          <>
+            <input class="text-black" {...props} type="email" required />
+            {field.error && <div>{field.error}</div>}
+          </>
+        )}
+      </Field>
+      <Field name="password">
+        {(field, props) => (
+          <>
+            <input class="text-black" {...props} type="password" required />
+            {field.error && <div>{field.error}</div>}
+          </>
+        )}
+      </Field>
+      <button type="submit" class="bg-blue-500 px-4 py-2">
+        Login
+      </button>
+    </Form>
+  );
+}
--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,7 +1,10 @@
+import { LoginForm } from "../components/login-form";
+
 export function Login() {
   return (
     <div>
       <h1>Login</h1>
+      <LoginForm />
     </div>
   );
 }
