1d5c1b6d430bf6ede071a42b826e47e07bafdaa8
dakribe <dakribe94@gmail.com>
2024-08-09 02:25:36 -0500

    Redirect authed users from login

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,6 +1,21 @@
+import { useNavigate } from "@solidjs/router";
 import { LoginForm } from "../components/login-form";
+import { useAuth } from "../providers/auth";
+import { createEffect } from "solid-js";
 
 export function Login() {
+  const { isAuthenticated, getUser } = useAuth();
+  const nav = useNavigate();
+
+  createEffect(async () => {
+    if (isAuthenticated()) {
+      const user = await getUser();
+      if (user) {
+        nav("/home", { replace: true });
+      }
+    }
+  });
+
   return (
     <div>
       <h1>Login</h1>
