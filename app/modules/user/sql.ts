c5038958f72668839f0de0263a0e670a3244441a
dalton <dakribe94@gmail.com>
2025-01-10 11:06:31 -0600

    update user schema

--- app/modules/user/sql.ts
+++ app/modules/user/sql.ts
@@ -1,10 +1,9 @@
-import { boolean, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
 	id: uuid("id").defaultRandom().primaryKey(),
 	email: varchar("email").notNull(),
 	username: varchar("username", { length: 15 }).unique(),
-	isOnboarded: boolean("is_onboarded").default(false).notNull(),
 });
 
 export type InsertUser = typeof user.$inferInsert;
