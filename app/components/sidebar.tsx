d5f10f550d1ac5700718390ef9f25e57b9a6cb9a
dalton <dakribe94@gmail.com>
2025-01-15 13:26:49 -0600

    add user to sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -10,6 +10,7 @@ import {
 	LucideIcon,
 } from "lucide-react";
 import { User } from "~/modules/user/sql";
+import { Avatar, AvatarFallback } from "./ui/avatar";
 
 interface SidebarItem {
 	label: string;
@@ -66,25 +67,37 @@ export function Sidebar({ user }: SidebarProps) {
 	}));
 
 	return (
-		<div className="h-screen w-64 p-4 border-r border-gray-200 flex flex-col">
-			<div className="mb-4">
-				<Link to="/home" className="text-2xl font-bold">
-					Memo
-				</Link>
+		<div className="h-screen w-64 p-4 border-r border-gray-200 flex flex-col justify-between">
+			<div>
+				<div className="mb-4">
+					<Link to="/home" className="text-2xl font-bold">
+						Memo
+					</Link>
+				</div>
+				<ul className="space-y-2">
+					{items.map((item) => (
+						<li key={item.label}>
+							<Link
+								to={item.url}
+								className="flex gap-4 items-center py-2 rounded-full"
+							>
+								{item.icon && <item.icon className="w-6 h-6" />}
+								<span className="text-lg">{item.label}</span>
+							</Link>
+						</li>
+					))}
+				</ul>
 			</div>
-			<ul className="space-y-2">
-				{items.map((item) => (
-					<li key={item.label}>
-						<Link
-							to={item.url}
-							className="flex gap-4 items-center py-2 rounded-full"
-						>
-							{item.icon && <item.icon className="w-6 h-6" />}
-							<span className="text-lg">{item.label}</span>
-						</Link>
-					</li>
-				))}
-			</ul>
+
+			<div className="mt-8 flex items-center gap-3">
+				<Avatar>
+					<AvatarFallback>{user.username?.slice(0, 1)}</AvatarFallback>
+				</Avatar>
+				<div>
+					<p className="font-semibold">{user.username}</p>
+					<p className="text-sm text-gray-500">@{user.username}</p>
+				</div>
+			</div>
 		</div>
 	);
 }
